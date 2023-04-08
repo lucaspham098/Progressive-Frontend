5aa572bbbbef0b05c64cd3c29c7b89b1461df84c
lucaspham098 <lucaspham098@gmail.com>
2023-04-07 23:45:44 -0400

    add delete exercise function

--- src/pages/ComparisonPage.jsx
+++ src/pages/ComparisonPage.jsx
@@ -63,13 +63,34 @@ const ComparisonPage = () => {
         window.location.reload()
     }
 
+    const handleDelete = (event) => {
+        const token = sessionStorage.getItem('JWTtoken');
+        const id = event.target.id
+        console.log(event.target.id)
+        axios
+            .delete(`${API_URL}/exercises/id/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err.response.data)
+            })
 
+        window.location.reload()
+    }
 
     return (
         <div>
             {modal && <Modal id={exerciseID} name={exerciseName} />}
             {exerciseList.map(exercise => {
-                return (<div key={exercise.id} id={exercise.id} data-name={exercise.exercise_name} onClick={handleClick}>{exercise.exercise_name}</div>
+                return (<div key={exercise.id} id={exercise.id} data-name={exercise.exercise_name} onClick={handleClick}>
+                    {exercise.exercise_name}
+                    <button onClick={handleDelete} id={exercise.id}>delete</button>
+                </div>
                 )
             })}
             <button onClick={handleAdd}>add new exercise</button>
